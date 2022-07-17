f8212760135bce337afcb2f107baefe471f8a77c
Karan Pal Singh <kdsgyt@gmail.com>
2022-07-17 01:45:15 -0400

    Created the pages that are required

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NothingComponent } from './pages/nothing/nothing.component';
+import { StoreComponent } from './pages/store/store.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    HomeComponent,
+    NothingComponent,
+    StoreComponent,
+    ProductsComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
