81380a379a197b7de987ca9c6d95fa693f7ed8cf
Karan Pal Singh <kdsgyt@gmail.com>
2022-07-24 13:07:29 -0400

    Basic functioning of the menubar is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,29 @@ import { NothingComponent } from './pages/nothing/nothing.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { StoreComponent } from './pages/store/store.component';
 
+
+/**
+ * lazy loadin can be implemented to decrease load time for specific routes as follow.
+ * 
+ */
+
+/**
+ * https://enlear.academy/how-to-implement-lazy-loading-in-angular-c3f445a2f379
+*/
+
 const routes: Routes = [
   { path: "", component: NothingComponent },
   { path: "products", component: ProductsComponent },
   { path: 'store', component: StoreComponent },
-  { path: 'login', component: LoginComponent }
+  { 
+    path: 'login', 
+    component: LoginComponent,
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
