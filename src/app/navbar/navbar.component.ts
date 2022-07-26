eb3e6a9c20c500a77fd013394bb038e5e40fccbb
Karan Pal Singh <kdsgyt@gmail.com>
2022-07-26 07:56:25 -0400

    Navbar is now complete just the links to work left for submenu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+  public previewFile: string = '';
+  // change the source to the preview file according to the hovered item
+  change(product: string): void {
+    this.previewFile = `./assets/landingPageAssets/submenu/${product}.jpg`
+  }
+
+  // set the inital value of all the variables.
   ngOnInit(): void {
+    this.previewFile = './assets/landingPageAssets/submenu/Header.jpg'
   }
 
 }
